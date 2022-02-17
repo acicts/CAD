6bcdb465f3edee1e2ccd4892c438680163bb1a95
chamindujs <chamindujs@gmail.com>
2022-02-17 15:05:12 +0530

    Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CodeFlow from "./pages/CodeFlow";
+import DimX from "./pages/DimX";
+import './App.css';
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/codeflow" element={<CodeFlow />} />
+			<Route path="/dimensionx" element={<DimX />} />
 		</Routes>
 	);
 };
---
+++ src/pages/DimX.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/DimensionX/Layout'
+import Home from '../components/DimensionX/Sections/Home'
+
+const DimX = () => {
+  return (
+    <div>
+        <Layout>
+            <Home />
+        </Layout>
+    </div>
+  )
+}
+
+export default DimX
