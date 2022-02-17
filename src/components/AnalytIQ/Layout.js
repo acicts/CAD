ebb206857f4067e4816f2ca0c783898a3cafb158
chamindujs <chamindujs@gmail.com>
2022-02-18 00:58:07 +0530

    Add AnalytIQ file structure

---
+++ src/components/AnalytIQ/Layout.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "../../styles/DimensionX/Layout.module.css";
+import data from "../../DimensionXData.json";
+import '../../pages/DimensionX.css'
+
+const Layout = ({ children }) => {
+	return (
+		<div className={classes.Container}>
+			<nav className={classes.NavBar}>
+				<div></div>
+				<ul>
+					{data.navbar.map((navItem) => {
+						return (
+							<li>
+								<a href={navItem.link}>{navItem.text}</a>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
+			<main>{children}</main>
+			
+		</div>
+	);
+};
+
+export default Layout;
