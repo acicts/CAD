a9744fa3a1d3c26f929647848291110265c5eb68
Shakya Peiris <shakyaimanjith32@gmail.com>
2022-02-18 19:54:26 +0530

    Add rules and FAQs

--- src/components/CodeFLow/FAQItem.js
+++ src/components/CodeFLow/FAQItem.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import React from "react";
+import ArrowRightIcon from "@material-ui/icons/ArrowRight";
 import classes from "../../styles/CodeFLow/FAQItem.module.css";
-
 
 const FAQItem = ({ title, body, index, currIndex, changeIndexHandler }) => {
 	return (
-		<div>
-			<div className={classes.TopContainer} onClick={() => { changeIndexHandler(index); }}>
-				<span><ArrowRightIcon /></span>
-				<span className={classes.Title}>{ title }</span>
+		<div className={classes.Container}>
+			<div
+				className={classes.TopContainer}
+				onClick={() => {
+					changeIndexHandler(index);
+				}}
+			>
+				<span
+					style={{
+						transform: currIndex === index ? "rotateZ(45deg)" : "rotateZ(0deg)",
+						transition: "0.2s all ease",
+					}}
+				>
+					<ArrowRightIcon />
+				</span>
+				<span className={classes.Title}>{title}</span>
 			</div>
-			{
-				currIndex === index && <div className={classes.BottomContainer}>
-					<div className={classes.Bar}><div></div></div>
-					<div className={classes.Text}><p>{ body }</p></div>
+			{currIndex === index && (
+				<div className={classes.BottomContainer}>
+					<div className={classes.Bar}>
+						<div></div>
+					</div>
+					<div className={classes.Text}>
+						<p>{body}</p>
+					</div>
 				</div>
-			}
+			)}
 		</div>
-	)
-}
+	);
+};
 
 export default FAQItem;
--- src/components/CodeFLow/sections/Rules.js
+++ src/components/CodeFLow/sections/Rules.js
@@ -1,25 +1,26 @@
-import React from 'react'
-import SectionTopic from '../SectionTopic'
+import React from "react";
+import SectionTopic from "../SectionTopic";
+import data from "../../../data.json";
 import classes from "../../../styles/CodeFLow/Rules.module.css";
 
 const Rules = () => {
-  return (
-    <section className={classes.Container} id="rules">
-    	<SectionTopic
-    		title={"Rules & Regulations"}
-    		color={"white"}
-    		pendulamColor={"white"}
-    	>
-    		<ol>
-    			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-    			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-    			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-    			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-    			<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-    		</ol>
-    	</SectionTopic>
-    </section>
-  )
-}
+	return (
+		<section className={classes.Container} id="rules">
+			<SectionTopic
+				title={"Rules & Regulations"}
+				color={"white"}
+				pendulamColor={"white"}
+				childrenStyle={{ height: "max-content" }}
+				containerStyles={{ height: "max-content" }}
+			>
+				<ol>
+					{data.codeflow.rules.map((rule) => (
+						<li>{rule}</li>
+					))}
+				</ol>
+			</SectionTopic>
+		</section>
+	);
+};
 
 export default Rules;
