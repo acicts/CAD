f58cc318f2d676d9288ceea59b7ce2ae43de0c5f
Shakya Peiris <shakyaimanjith32@gmail.com>
2022-02-18 13:36:15 +0530

    Add responsive navbar to DimensionX

--- src/components/DimensionX/Layout.js
+++ src/components/DimensionX/Layout.js
@@ -1,14 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import Menu from "@material-ui/icons/Menu";
+import { motion } from "framer-motion";
 import classes from "../../styles/DimensionX/Layout.module.css";
 import data from "../../data.json";
-import '../../pages/DimensionX.css'
+import "../../pages/DimensionX.css";
 
 const Layout = ({ children }) => {
+	const [isNav, setIsNav] = useState(false);
+	const hamburgerVariants = {
+		clicked: {
+			left: "40%",
+			color: "#33b161",
+			zIndex: 20,
+			transition: {
+				duration: 0.3,
+				ease: "easeOut",
+			},
+		},
+		notClicked: {
+			left: "0%",
+			color: "#595959",
+			zIndex: 0,
+			transition: {
+				duration: 0.3,
+				ease: "easeOut",
+			},
+		},
+	};
+
+	const navbarVariants = {
+		clicked: {
+			scale: 1,
+			opacity: 1,
+			left: 0,
+			top: 0,
+			transition: {
+				duration: 0.3,
+				ease: "easeOut",
+			},
+		},
+		notClicked: {
+			scale: 0,
+			opacity: 0,
+			left: "-20%",
+			top: "-100vh",
+			transition: {
+				duration: 0.3,
+				ease: "easeOut",
+			},
+		},
+	};
 	return (
 		<div className={classes.Container}>
 			<nav className={classes.NavBar}>
-				<div></div>
-				<ul>
+				<div className={classes.TempBox}></div>
+				<div className={classes.NavBarMenu}>
+					<motion.div
+						onClick={() => {
+							setIsNav((curr) => !curr);
+						}}
+						style={{ position: "relative" }}
+						variants={hamburgerVariants}
+						animate={isNav ? "clicked" : "notClicked"}
+						initial={"notClicked"}
+					>
+						<Menu color="#595959" />
+					</motion.div>
+				</div>
+
+				<ul className={classes.PcNavbar}>
 					{data.DimensionX.navbar.map((navItem) => {
 						return (
 							<li>
@@ -17,9 +77,36 @@ const Layout = ({ children }) => {
 						);
 					})}
 				</ul>
+				<div className={classes.RespNav}>
+					<motion.ul
+						variants={navbarVariants}
+						animate={isNav ? "clicked" : "notClicked"}
+						initial={"notClicked"}
+					>
+						{data.DimensionX.navbar.map((navItem) => {
+							return (
+								<li>
+									<a
+										onClick={() => {
+											setIsNav(false);
+										}}
+										href={navItem.link}
+									>
+										{navItem.text}
+									</a>
+								</li>
+							);
+						})}
+					</motion.ul>
+				</div>
 			</nav>
-			<main>{children}</main>
-			
+			<main
+				onClick={() => {
+					if (isNav) setIsNav(false);
+				}}
+			>
+				{children}
+			</main>
 		</div>
 	);
 };
