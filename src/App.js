5b146cb6ece417e9999737e202694de85dca1f22
Shakya Peiris <shakyaimanjith32@gmail.com>
2022-02-19 20:55:48 +0530

    Implement lazy loading for home page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Loading from "./components/Loading";
 import "./App.css";
-import CAD from "./pages/CAD";
 
 const CodeFlow = React.lazy(() => import("./pages/CodeFlow"));
 const DimX = React.lazy(() => import("./pages/DimX"));
 const AnalytIQ = React.lazy(() => import("./pages/AnalytIQ"));
+const CAD = React.lazy(() => import("./pages/CAD"));
 
 const App = () => {
 	return (
 		<Suspense fallback={<Loading />}>
 			<Routes>
-				<Route path="/" exact element={<CAD />} />
+				<Route path="/" element={<CAD />} />
 				<Route path="/codeflow" element={<CodeFlow />} />
 				<Route path="/dimensionx" element={<DimX />} />
 				<Route path="/analytiq" element={<AnalytIQ />} />
