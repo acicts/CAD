3820438eb3393072adf7dd5fe544f88b77b29b8a
Janiru <janiru160@gmail.com>
2022-02-20 16:59:55 +0530

    modified target field in the faq section links (CodeFlow)

--- src/components/CodeFLow/FAQItem.js
+++ src/components/CodeFLow/FAQItem.js
@@ -4,53 +4,59 @@ import { motion } from "framer-motion";
 import classes from "../../styles/CodeFLow/FAQItem.module.css";
 
 const FAQItem = ({
-	title,
-	body,
-	index,
-	currIndex,
-	changeIndexHandler,
-	link,
+  title,
+  body,
+  index,
+  currIndex,
+  changeIndexHandler,
+  link,
+  target,
 }) => {
-	const animationVariants = {
-		clicked: {
-			transform: "rotateZ(45deg)",
-			display: "inline-block",
-		},
-		notClicked: {
-			display: "inline-block",
-			transform: "rotateZ(0deg)",
-		},
-	};
-	return (
-		<div className={classes.Container}>
-			<div
-				className={classes.TopContainer}
-				onClick={() => {
-					changeIndexHandler(index);
-				}}
-			>
-				<motion.span
-					variants={animationVariants}
-					animate={index === currIndex ? "clicked" : "notClicked"}
-				>
-					<ArrowRightIcon />
-				</motion.span>
-				<span className={classes.Title}>{title}</span>
-			</div>
-			{currIndex === index && (
-				<div className={classes.BottomContainer}>
-					<div className={classes.Bar}>
-						<div></div>
-					</div>
-					<div className={classes.Text}>
-						<p>
-							{body} {link && <a href={link.ref}>{link.text}</a>}
-						</p>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+  const animationVariants = {
+    clicked: {
+      transform: "rotateZ(45deg)",
+      display: "inline-block",
+    },
+    notClicked: {
+      display: "inline-block",
+      transform: "rotateZ(0deg)",
+    },
+  };
+  return (
+    <div className={classes.Container}>
+      <div
+        className={classes.TopContainer}
+        onClick={() => {
+          changeIndexHandler(index);
+        }}
+      >
+        <motion.span
+          variants={animationVariants}
+          animate={index === currIndex ? "clicked" : "notClicked"}
+        >
+          <ArrowRightIcon />
+        </motion.span>
+        <span className={classes.Title}>{title}</span>
+      </div>
+      {currIndex === index && (
+        <div className={classes.BottomContainer}>
+          <div className={classes.Bar}>
+            <div></div>
+          </div>
+          <div className={classes.Text}>
+            <p>
+              {body}{" "}
+              {link && (
+                <a href={link.ref} target={link.target}>
+                  {link.text}
+                </a>
+              )}
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default FAQItem;
--- src/components/CodeFLow/sections/FAQ.js
+++ src/components/CodeFLow/sections/FAQ.js
@@ -5,40 +5,41 @@ import classes from "../../../styles/CodeFLow/FAQ.module.css";
 import data from "../../../data.json";
 
 const FAQ = () => {
-	const textColor = "#07253a";
-	const pendulamColor = "#07253a";
-	const [currIndex, setCurrIndex] = useState(null);
+  const textColor = "#07253a";
+  const pendulamColor = "#07253a";
+  const [currIndex, setCurrIndex] = useState(null);
 
-	const changeIndexHandler = (index) => {
-		console.log(index);
-		if (index === currIndex) {
-			setCurrIndex(null);
-		} else setCurrIndex(index);
-	};
-	return (
-		<section className={classes.Container} id="faq">
-			<SectionTopic
-				title={"Frequently Asked Questions"}
-				color={textColor}
-				pendulamColor={pendulamColor}
-			>
-				<div className={classes.QuestionContainer}>
-					{data.codeflow.faq.map((item, i) => {
-						return (
-							<FAQItem
-								title={item.question}
-								body={item.Answer}
-								index={i}
-								currIndex={currIndex}
-								link={item.link}
-								changeIndexHandler={changeIndexHandler}
-							/>
-						);
-					})}
-				</div>
-			</SectionTopic>
-		</section>
-	);
+  const changeIndexHandler = (index) => {
+    console.log(index);
+    if (index === currIndex) {
+      setCurrIndex(null);
+    } else setCurrIndex(index);
+  };
+  return (
+    <section className={classes.Container} id="faq">
+      <SectionTopic
+        title={"Frequently Asked Questions"}
+        color={textColor}
+        pendulamColor={pendulamColor}
+      >
+        <div className={classes.QuestionContainer}>
+          {data.codeflow.faq.map((item, i) => {
+            return (
+              <FAQItem
+                title={item.question}
+                body={item.Answer}
+                index={i}
+                currIndex={currIndex}
+                link={item.link}
+                changeIndexHandler={changeIndexHandler}
+                target={item.target}
+              />
+            );
+          })}
+        </div>
+      </SectionTopic>
+    </section>
+  );
 };
 
 export default FAQ;
