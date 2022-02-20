aa574663cd8fa064d02140a021cdf833c88f9dd5
Janiru <janiru160@gmail.com>
2022-02-20 14:03:31 +0530

    typo fix in AnalytIQ footer

--- src/components/AnalytIQ/Sections/Footer.js
+++ src/components/AnalytIQ/Sections/Footer.js
@@ -2,18 +2,18 @@ import React from "react";
 import classes from "../../../styles/AnalytIQ/Footer.module.css";
 
 const Footer = () => {
-	return (
-		<div className={classes.aboutSection}>
-			<div className={classes.aboutContainer}>
-				<div className={classes.description}>
-					<p>
-						A Projects By <a href="/link">ACICTS</a>
-					</p>
-					<p>Copyright reserved 2022</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={classes.aboutSection}>
+      <div className={classes.aboutContainer}>
+        <div className={classes.description}>
+          <p>
+            A Project By <a href="/link">ACICTS</a>
+          </p>
+          <p>Copyright reserved 2022</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
