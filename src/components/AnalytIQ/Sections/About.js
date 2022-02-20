d2e3eb9787209171c571d43e68d84d49a8cde827
Janiru <janiru160@gmail.com>
2022-02-20 10:10:19 +0530

    update details

--- src/components/AnalytIQ/Sections/About.js
+++ src/components/AnalytIQ/Sections/About.js
@@ -11,14 +11,8 @@ const About = () => {
 
         <div className={classes.description}>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-            morbi vestibulum morbi sapien amet etiam euismod.
-          </p>
-          <p>
-            Vitae ipsum vulputate nec nec dui urna, nec neque. Volutpat
-            vulputate vestibulum mauris mauris sed fermentum. Habitasse sem
-            facilisi semper bibendum quam nunc in. Nulla nec porttitor morbi
-            faucibus felis, et, tincidunt donec id.
+            AnalytIQ is an online inter-school team quiz competition organized
+            by the ICT Society of Ananda College.
           </p>
         </div>
       </div>
