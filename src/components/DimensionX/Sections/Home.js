dc08b24af6f00a74b1dd02a4675825478cab7f3a
Shakya Peiris <shakyaimanjith32@gmail.com>
2022-02-20 21:09:17 +0530

    Fix responsive issue of DimX

--- src/components/DimensionX/Sections/Home.js
+++ src/components/DimensionX/Sections/Home.js
@@ -2,30 +2,32 @@ import React from "react";
 import classes from "../../../styles/DimensionX/Home.module.css";
 
 const Home = () => {
-  return (
-    <div className={classes.homeContainer} id={"home"}>
-      <div className={classes.mainTitleWrapper}>
-        <div className={classes.titleContainer}>
-          <h1 className={classes.title}>DIMENSION</h1>
-        </div>
-      </div>
+	return (
+		<div className={classes.homeContainer} id={"home"}>
+			<div className={classes.mainTitleWrapper}>
+				<div className={classes.titleContainer}>
+					<h1 className={classes.title}>DIMENSION</h1>
+				</div>
+			</div>
 
-      <div className={classes.subTextContainer}>
-        <p>An Inter-School</p>
-        <p>2D & 3D Graphic Design Competition</p>
-        <p>Organized by</p>
-        <p>The ICT Society of Ananda College</p>
-      </div>
+			<div className={classes.subTextContainer}>
+				<p>An Inter-School</p>
+				<p>
+					2D & 3D <br /> Graphic Design Competition
+				</p>
+				<p>Organized by</p>
+				<p>The ICT Society of Ananda College</p>
+			</div>
 
-      <div className={classes.buttonContainer}>
-        <div className={classes.button}>
-          <a href="#faq">
-            Start <br></br> Creating
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+			<div className={classes.buttonContainer}>
+				<div className={classes.button}>
+					<a href="#faq">
+						Start <br></br> Creating
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
