093d01725a8c5b9e1ef69cf21977a307dd9b0d46
Janiru <janiru160@gmail.com>
2022-02-21 13:29:03 +0530

    typo

--- src/components/DimensionX/Sections/Rules.js
+++ src/components/DimensionX/Sections/Rules.js
@@ -4,54 +4,54 @@ import AboutImage from "../../../images/DimensionX/banner2.svg";
 import data from "../../../data.json";
 
 const Rules = () => {
-	return (
-		<div className={classes.rulesSection} id={"rules"}>
-			<img src={AboutImage} className={classes.image} alt="Dimx-cover" />
-			<div className={classes.rulesContainer}>
-				<div className={classes.title}>
-					<p>Rules and</p>
-					<h1>Regulations</h1>
-				</div>
+  return (
+    <div className={classes.rulesSection} id={"rules"}>
+      <img src={AboutImage} className={classes.image} alt="Dimx-cover" />
+      <div className={classes.rulesContainer}>
+        <div className={classes.title}>
+          <p>Rules and</p>
+          <h1>Regulations</h1>
+        </div>
 
-				<div className={classes.description}>
-					<div className={classes.singleCategory}>
-						<h1>3D</h1>
-						<ul className={classes.rules}>
-							{data.DimensionX.rules.threeD.map((rule) => {
-								return <li>{rule}</li>;
-							})}
-						</ul>
-					</div>
+        <div className={classes.description}>
+          <div className={classes.singleCategory}>
+            <h1>3D</h1>
+            <ul className={classes.rules}>
+              {data.DimensionX.rules.threeD.map((rule) => {
+                return <li>{rule}</li>;
+              })}
+            </ul>
+          </div>
 
-					<div className={classes.singleCategory}>
-						<h1>Photo manipulation</h1>
-						<ul className={classes.rules}>
-							{data.DimensionX.rules.twoD.map((rule) => {
-								return <li>{rule}</li>;
-							})}
-						</ul>
-					</div>
+          <div className={classes.singleCategory}>
+            <h1>Photo Manipulation</h1>
+            <ul className={classes.rules}>
+              {data.DimensionX.rules.twoD.map((rule) => {
+                return <li>{rule}</li>;
+              })}
+            </ul>
+          </div>
 
-					<div className={classes.singleCategory}>
-						<h1>Logo Designing</h1>
-						<ul className={classes.rules}>
-							{data.DimensionX.rules.logo.map((rule) => {
-								return <li>{rule}</li>;
-							})}
-						</ul>
-					</div>
-					<div className={classes.singleCategory}>
-						<h1>Poster Design</h1>
-						<ul className={classes.rules}>
-							{data.DimensionX.rules.poster.map((rule) => {
-								return <li>{rule}</li>;
-							})}
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+          <div className={classes.singleCategory}>
+            <h1>Logo Designing</h1>
+            <ul className={classes.rules}>
+              {data.DimensionX.rules.logo.map((rule) => {
+                return <li>{rule}</li>;
+              })}
+            </ul>
+          </div>
+          <div className={classes.singleCategory}>
+            <h1>Poster Design</h1>
+            <ul className={classes.rules}>
+              {data.DimensionX.rules.poster.map((rule) => {
+                return <li>{rule}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rules;
