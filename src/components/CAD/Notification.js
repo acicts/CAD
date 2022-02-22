a7e894313339b9466073aa17c7f24f4a99bbddfc
Shakya Peiris <shakyaimanjith32@gmail.com>
2022-02-22 11:47:33 +0530

    Add notification to homepage

---
+++ src/components/CAD/Notification.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import Close from "@material-ui/icons/Close";
+import classes from "../../styles/CAD/Notification.module.css";
+import { motion, useAnimation } from "framer-motion";
+const Notification = () => {
+	const animationVariants = {
+		initial: {
+			left: "72%",
+		},
+		close: {
+			left: "125%",
+		},
+	};
+	const controller = useAnimation();
+	useEffect(() => {
+		controller.start("initial");
+	}, []);
+	return (
+		<motion.div
+			animate={controller}
+			variants={animationVariants}
+			initial={"close"}
+			className={classes.Container}
+		>
+			<span
+				onClick={() => {
+					controller.start("close");
+				}}
+			>
+				<Close />
+			</span>
+			<p>Anyone can participate in more than one compeition.</p>
+		</motion.div>
+	);
+};
+
+export default Notification;
--- src/pages/CAD.js
+++ src/pages/CAD.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Layout from "../components/CodeFLow/Layout";
 import Home from "../components/CAD/Sections/Home";
+import Notification from "../components/CAD/Notification";
 
 const CAD = () => {
-  return <Home />;
+	return (
+		<>
+			<Home />
+			<Notification />
+		</>
+	);
 };
 
 export default CAD;
