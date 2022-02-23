77d6c56b7aeb172860209c23f0e174991a88b7a9
Janiru <janiru160@gmail.com>
2022-02-23 14:01:26 +0530

    added confetti to prizes section DimX

--- src/components/DimensionX/Sections/Prizes.js
+++ src/components/DimensionX/Sections/Prizes.js
@@ -2,18 +2,29 @@ import React from "react";
 import classes from "../../../styles/DimensionX/Prizes.module.css";
 import SectionTopic from "../SectionTopic";
 import data from "../../../data.json";
-import { Link } from "react-router-dom";
+
+import Confetti from 'react-confetti'
+
 
 function Prizes() {
+  const { innerWidth: width, innerHeight: height } = window;
   return (
-    <section className={classes.container} id={"prizes"}>
-      <SectionTopic title={"Prizes"} color={"46dd9a"} pendulamColor={"#46dd9a"}>
+    <div className={classes.container}>
+    
+         <SectionTopic title={"Prizes"} color={"46dd9a"} pendulamColor={"#46dd9a"}>
         <div className={classes.textContainer}>
+          
           <p className={classes.title}>{data.DimensionX.prizes.prize}</p>
         </div>
         <div className={classes.imageContainer}></div>
       </SectionTopic>
-    </section>
+      <Confetti
+      width={width}
+      height={height}
+      colors={['#1cd49f']}
+      numberOfPieces={"160"}
+    />
+   </div>
   );
 }
 export default Prizes;
