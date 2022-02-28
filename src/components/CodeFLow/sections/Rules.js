d0aef91ed0e0f4404074f79af1f54d799d722cc4
Shakya Peiris <shakyaimanjith32@gmail.com>
2022-03-01 00:52:09 +0530

    Add rule

--- src/components/CodeFLow/sections/Rules.js
+++ src/components/CodeFLow/sections/Rules.js
@@ -17,6 +17,16 @@ const Rules = () => {
 					{data.codeflow.rules.map((rule) => (
 						<li>{rule}</li>
 					))}
+					<li>
+						You should make a video that is no longer than 5 minutes about your
+						project. If you are not sure what to include in the video, read
+						<a
+							href="https://drive.google.com/file/d/1h6MJs39Rxi15sxlVvXp3YurzDTDmzQGr/view?usp=sharing"
+							target="_blank"
+						>
+							{" this"}
+						</a>
+					</li>
 				</ol>
 			</SectionTopic>
 		</section>
