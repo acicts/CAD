2d9240019b5c8dbd81bdaf9ed4c67ec149b65e88
Shakya Peiris <shakyaimanjith32@gmail.com>
2022-03-01 01:30:07 +0530

    Fix issue with spacing

--- src/components/CodeFLow/sections/Rules.js
+++ src/components/CodeFLow/sections/Rules.js
@@ -18,13 +18,13 @@ const Rules = () => {
 						<li>{rule}</li>
 					))}
 					<li>
-						You should make a video that is no longer than 5 minutes about your
-						project. If you are not sure what to include in the video, read
+						{`You should make a video that is no longer than 5 minutes about your
+						project. If you are not sure what to include in the video, read `}
 						<a
 							href="https://drive.google.com/file/d/1h6MJs39Rxi15sxlVvXp3YurzDTDmzQGr/view?usp=sharing"
 							target="_blank"
 						>
-							{" this"}
+							{"this"}
 						</a>
 					</li>
 				</ol>
