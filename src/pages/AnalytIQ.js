16ec250a66cd1f3d3fdd8ee2bb4e8e7cc1c8684b
Janiru <janiru160@gmail.com>
2022-03-06 19:17:10 +0530

    second round qualifiers section added

--- src/pages/AnalytIQ.js
+++ src/pages/AnalytIQ.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import Layout from '../components/AnalytIQ/Layout'
-import About from '../components/AnalytIQ/Sections/About'
-import Contact from '../components/AnalytIQ/Sections/Contact'
-import Footer from '../components/AnalytIQ/Sections/Footer'
-import Home from '../components/AnalytIQ/Sections/Home'
-import Rules from '../components/AnalytIQ/Sections/Rules'
-
+import React from "react";
+import Layout from "../components/AnalytIQ/Layout";
+import About from "../components/AnalytIQ/Sections/About";
+import Contact from "../components/AnalytIQ/Sections/Contact";
+import Footer from "../components/AnalytIQ/Sections/Footer";
+import Home from "../components/AnalytIQ/Sections/Home";
+import Rules from "../components/AnalytIQ/Sections/Rules";
+import Winners from "../components/AnalytIQ/Sections/Winners";
 const AnalytIQ = () => {
   return (
     <div>
-        <Layout>
-            <Home />
-            <About />
-            <Rules />
-            <Contact />
-            <Footer />
-        </Layout>
+      <Layout>
+        <Home />
+        <About />
+        <Rules />
+        <Winners />
+        <Contact />
+        <Footer />
+      </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default AnalytIQ
+export default AnalytIQ;
